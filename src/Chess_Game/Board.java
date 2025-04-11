3d472e16df4c4d7b118eefbbdb02e07db54b8cfa
unknown <bedianashvili.nitsa@kiu.edu.ge>
2025-04-11 20:42:04 +0400

    added a getSquare method that will return the object on specified positions

--- src/Chess_Game/Board.java
+++ src/Chess_Game/Board.java
@@ -3,7 +3,7 @@ package Chess_Game;
 public class Board {
 
 //        [
-//            [ [0][0], [0][1], [0][2], [0][3], [0][4], [0][5], [0][6], [0][7] ], // Row 0   blacks
+//            [ [0][0], [0][1], [0][2], [0][3], [0][4], [0][5], [0][6], [0][7] ], // Row 0   blacks   [  [8][a]  ,[8][b] ...
 //            [ [1][0], [1][1], [1][2], [1][3], [1][4], [1][5], [1][6], [1][7] ], // Row 1
 //            [ [2][0], [2][1], [2][2], [2][3], [2][4], [2][5], [2][6], [2][7] ], // Row 2
 //            [ [3][0], [3][1], [3][2], [3][3], [3][4], [3][5], [3][6], [3][7] ], // Row 3
@@ -12,11 +12,36 @@ public class Board {
 //            [ [6][0], [6][1], [6][2], [6][3], [6][4], [6][5], [6][6], [6][7] ], // Row 6
 //            [ [7][0], [7][1], [7][2], [7][3], [7][4], [7][5], [7][6], [7][7] ]  // Row 7   whites
 //            ]
-    private  Object [][] board  ;
+    private  Figurine [][] board  ;
+    public Figurine[][] getBoard() {
+        return board;
+    }
+
+    public Figurine getSquare(int positionX , int positionY ){
+        return this.getBoard()[positionX][positionY];
+    }
+
+    public Figurine getSquare(String positionX , String positionY ){
+        int posX=0;
+        int posY=0;
+        switch (positionX){
+            case "a" : {break;}
+            case "b" : {posY=1;break;}
+            case "c" : {posY=2;break;}
+            case "d" : {posY=3;break;}
+            case "e" : {posY=4;break;}
+            case "f" : {posY=5;break;}
+            case "g" : {posY=6;break;}
+            case "h" : {posY=7;break;}
+        }
+        posX=8-Integer.parseInt(positionY);
+        return getSquare(posX,posY);
+    }
+
 
     public Board() {
 
-        board= new Object[7][7];
+        board= new Figurine[8][8];
         for (int x = 0; x < 8; x++) {
             board[1][x]=new Pawn("w");
             board[6][x]=new Pawn("b");
--- src/Chess_Game/Figurine.java
+++ src/Chess_Game/Figurine.java
@@ -6,7 +6,7 @@ public abstract class Figurine {
 //    logic- in every specific figurine will be a checker() which will check if for a specific position
 //    there are available pieces that could move there. if there are the Board object will be updated accordingly
 //    ie the old position will be wiped and the new one will be updated
-    public abstract boolean moveCorrectWhite(int positionFirst ,int PositionSecond);
+    public abstract boolean moveCorrectWhite(int positionFirst ,int PositionSecond);  //ie position 8a = [0][0]
 
 
 }
